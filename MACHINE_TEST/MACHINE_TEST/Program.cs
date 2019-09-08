90a7bebcb00f4f9e403c6435c61ebccb3d92f9c8
Pooja-Gaikwad <gaikwadpooja0921@gmail.com>
2019-09-08 12:36:42 +0530

    I have added project

---
+++ MACHINE_TEST/MACHINE_TEST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MACHINE_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+    }
+    class A()
+    {
+    public int ID { get; set; }
+    }
+}
